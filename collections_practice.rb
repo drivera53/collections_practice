8a66a692c9ffe6d888eb3149297ff5265fd58d64
Danny Rivera <samboard@hotmail.com>
2021-02-06 15:12:55 -0500

    commit tree a1340870a16daf1d64eae730a1d9017edaf06857

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+require 'pry'
 
+def sort_array_asc(array)
+    return array.sort 
+end
+
+def sort_array_desc(array)
+    return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    return array.sort_by(&:length)
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def swap_elements_from_to(array, first, second)
+    array[first], array[second] = array[second], array[first]
+    return array
+end
+
+def reverse_array(array)
+    return array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |word|
+        word[2] = "$"
+    end
+    return array
+end
+
+def find_a(array)
+    new_array = array.select do |word|
+        word.start_with? "a"
+    end
+    return new_array
+end
+
+def sum_array(array)
+    sum = 0
+    array.each do |number|
+        sum += number
+    end
+    return sum
+end
+
+def add_s(array)
+    array.each_with_index.collect{|element, index|
+        if index == 1
+            element
+        else
+            element << "s"
+        end
+    }
+    return array
+end
+
+#binding.pry
